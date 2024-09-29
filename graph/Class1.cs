fc98341e04f3ee6af3f2db130c288c918d429eb4
BOTYA <95568178+botyak1234@users.noreply.github.com>
2024-09-29 22:29:19 +0400

    Update Class1.cs

--- graph/Class1.cs
+++ graph/Class1.cs
@@ -1,24 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace graph
 {
     internal class Graph
     {
-        private Dictionary<int, List<Tuple<int, int>>> adjacencyList;
+        private Dictionary<string, List<Tuple<string, int>>> adjacencyList;
         private bool isDirected;
         private bool isWeighted;
         // Конструктор по умолчанию: создаёт пустой граф, ориентированный или неориентированный
         public Graph(bool isDirected = true)
         {
-            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            adjacencyList = new Dictionary<string, List<Tuple<string, int>>>();
             this.isDirected = isDirected;
         }
 
         public Graph(bool isDirected, bool isWeighted)
         {
-            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            adjacencyList = new Dictionary<string, List<Tuple<string, int>>>();
             this.isDirected = isDirected;
             this.isWeighted = isWeighted;
         }
@@ -27,7 +28,7 @@ namespace graph
         // Конструктор для графа из файла
         public Graph(string input)
         {
-            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            adjacencyList = new Dictionary<string, List<Tuple<string, int>>>();
 
             using (StreamReader file = new StreamReader(input))
             {
@@ -63,13 +64,13 @@ namespace graph
                         continue;
                     }
 
-                    int startVertex = int.Parse(parts[0]);
-                    int endVertex = int.Parse(parts[1]);
+                    string startVertex = parts[0];
+                    string endVertex = parts[1];
 
                     int weight = isWeighted ? int.Parse(parts[2]) : 1;
 
                     // Добавляем ребро
-                    addEdge(startVertex, endVertex, weight);
+                    addEdge_const(startVertex, endVertex, weight);
                 }
             }
         }
@@ -77,21 +78,48 @@ namespace graph
         // Конструктор-копия
         public Graph(Graph otherGraph)
         {
-            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            adjacencyList = new Dictionary<string, List<Tuple<string, int>>>();
             this.isDirected = otherGraph.isDirected;
             foreach (var vertex in otherGraph.adjacencyList)
             {
-                adjacencyList[vertex.Key] = new List<Tuple<int, int>>(vertex.Value);
+                adjacencyList[vertex.Key] = new List<Tuple<string, int>>(vertex.Value);
             }
+        }
+
+        private bool edgeExists(string startVertex, string endVertex)
+        {
+            if (adjacencyList.ContainsKey(startVertex))
+            {
+                foreach (var edge in adjacencyList[startVertex])
+                {
+                    if (edge.Item1 == endVertex)
+                    {
+                        return true; // Ребро уже существует
+                    }
+                }
+            }
+            return false; // Ребро не найдено
         }
 
         // Добавить ребро (дугу)
         // Добавить ребро (дугу)
-        public void addEdge(int startVertex, int endVertex, int weight = 1)
+        public void addEdge(string startVertex, string endVertex, int weight)
         {
+            if (edgeExists(startVertex, endVertex))
+            {
+                Console.WriteLine($"Ребро между вершинами {startVertex} и {endVertex} уже существует. Добавление нового ребра запрещено.");
+                return;
+            }
+            // Если граф взвешенный и вес не указан, выбрасываем ошибку
+            if (isWeighted && weight == null)
+            {
+                Console.WriteLine("Для взвешенного графа необходимо указать вес ребра.");
+                return;
+            }
+
             if (!isWeighted)
             {
-                weight = 1; // Если граф невзвешенный, устанавливаем вес в 1
+                weight = -1; // Если граф невзвешенный, устанавливаем вес в 1
             }
 
             if (!adjacencyList.ContainsKey(startVertex))
@@ -102,7 +130,7 @@ namespace graph
                 }
                 else
                 {
-                    adjacencyList[startVertex] = new List<Tuple<int, int>>();
+                    adjacencyList[startVertex] = new List<Tuple<string, int>>();
                 }
             }
             if (!adjacencyList.ContainsKey(endVertex))
@@ -111,12 +139,12 @@ namespace graph
             }
             else
             {
-                adjacencyList[startVertex].Add(new Tuple<int, int>(endVertex, weight));
+                adjacencyList[startVertex].Add(new Tuple<string, int>(endVertex, weight));
             }
             
             
             // Если граф неориентированный, добавляем обратное ребро
-            if (!isDirected)
+            if (!isDirected && startVertex != endVertex)
             {
                 if (!adjacencyList.ContainsKey(endVertex))
                 {
@@ -127,7 +155,7 @@ namespace graph
 
                     else
                     {
-                        adjacencyList[endVertex] = new List<Tuple<int, int>>();
+                        adjacencyList[endVertex] = new List<Tuple<string, int>>();
                     }
 
                 }
@@ -137,42 +165,42 @@ namespace graph
                 }
                 else
                 {
-                    adjacencyList[endVertex].Add(new Tuple<int, int>(startVertex, weight));
+                    adjacencyList[endVertex].Add(new Tuple<string, int>(startVertex, weight));
                 }
             }
         }
 
 
-        public void addEdge_const(int startVertex, int endVertex, int weight = 1)
+        public void addEdge_const(string startVertex, string endVertex, int weight = 1)
         {
             if (!isWeighted)
             {
-                weight = 1; // Если граф невзвешенный, устанавливаем вес в 1
+                weight = -1; // Если граф невзвешенный, устанавливаем вес в 1
             }
 
             if (!adjacencyList.ContainsKey(startVertex))
             {
-                adjacencyList[startVertex] = new List<Tuple<int, int>>();
+                adjacencyList[startVertex] = new List<Tuple<string, int>>();
             }
-            adjacencyList[startVertex].Add(new Tuple<int, int>(endVertex, weight));
+            adjacencyList[startVertex].Add(new Tuple<string, int>(endVertex, weight));
             // Если граф неориентированный, добавляем обратное ребро
-            if (!isDirected)
+            if (!isDirected && startVertex != endVertex)
             {
                 if (!adjacencyList.ContainsKey(endVertex))
                 {
-                    adjacencyList[endVertex] = new List<Tuple<int, int>>();
+                    adjacencyList[endVertex] = new List<Tuple<string, int>>();
                 }
-                adjacencyList[endVertex].Add(new Tuple<int, int>(startVertex, weight));
+                adjacencyList[endVertex].Add(new Tuple<string, int>(startVertex, weight));
             }
         }
 
 
         // Добавить вершину
-        public void addVertex(int vertex, List<Tuple<int, int>> edges = null)
+        public void addVertex(string vertex, List<Tuple<string, int>> edges = null)
         {
             if (!adjacencyList.ContainsKey(vertex))
             {
-                adjacencyList[vertex] = edges ?? new List<Tuple<int, int>>();
+                adjacencyList[vertex] = edges ?? new List<Tuple<string, int>>();
             }
             else
             {
@@ -183,17 +211,17 @@ namespace graph
         
 
         // Удаление ребра
-        public bool removeEdge(int startVertex, int endVertex, int weight)
+        public bool removeEdge(string startVertex, string endVertex, int weight)
         {
             if (adjacencyList.ContainsKey(startVertex))
             {
-                Tuple<int, int> edge = new Tuple<int, int>(endVertex, weight);
+                Tuple<string, int> edge = new Tuple<string, int>(endVertex, weight);
                 bool removed = adjacencyList[startVertex].Remove(edge);
 
                 // Если граф неориентированный, удаляем и обратное ребро
                 if (!isDirected && removed)
                 {
-                    adjacencyList[endVertex].Remove(new Tuple<int, int>(startVertex, weight));
+                    adjacencyList[endVertex].Remove(new Tuple<string, int>(startVertex, weight));
                 }
 
                 return removed;
@@ -201,7 +229,7 @@ namespace graph
             return false;
         }
         // Удаление вершины
-        public bool removeVertex(int vertex)
+        public bool removeVertex(string vertex)
         {
             if (!adjacencyList.ContainsKey(vertex))
             {
@@ -221,31 +249,81 @@ namespace graph
         }
 
         // Вывод списка смежности в файл
-        public void saveToFile(string outputFilePath)
+        public void saveToFile(string fileName)
         {
-            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            using (StreamWriter writer = new StreamWriter(fileName))
             {
+                // Записываем тип графа: ориентированный или нет, взвешенный или нет
+                writer.WriteLine($"isDirected:{isDirected}");
+                writer.WriteLine($"isWeighted:{isWeighted}");
+
+                // Записываем рёбра графа
                 foreach (var vertex in adjacencyList)
                 {
                     foreach (var edge in vertex.Value)
                     {
-                        writer.WriteLine($"{vertex.Key} {edge.Item1} {edge.Item2}");
+                        if (isWeighted)
+                        {
+                            // Формат записи для взвешенного графа: стартовая вершина, конечная вершина, вес
+                            writer.WriteLine($"{vertex.Key} {edge.Item1} {edge.Item2}");
+                        }
+                        else
+                        {
+                            // Формат записи для невзвешенного графа: стартовая вершина, конечная вершина
+                            writer.WriteLine($"{vertex.Key} {edge.Item1}");
+                        }
+                    }
+                }
+
+                // Находим изолированные вершины, которых нет в списке смежности или у которых нет рёбер
+                var allVertices = adjacencyList.Keys.Union(adjacencyList.Values.SelectMany(v => v.Select(e => e.Item1))).Distinct();
+                var isolatedVertices = allVertices.Where(v => !adjacencyList.ContainsKey(v) || adjacencyList[v].Count == 0);
+
+                // Записываем информацию об изолированных вершинах
+                if (isolatedVertices.Any())
+                {
+                    writer.WriteLine("Isolated vertices:");
+                    foreach (var isolatedVertex in isolatedVertices)
+                    {
+                        writer.WriteLine(isolatedVertex);
                     }
                 }
             }
+            Console.WriteLine($"Граф успешно сохранён в файл {fileName}");
         }
 
         // Вывод графа в консоль
         public void printGraph()
         {
-            foreach (var vertex in adjacencyList)
+            // Получаем список всех вершин (ключей) из adjacencyList
+            var allVertices = adjacencyList.Keys.ToList();
+
+            // Добавляем вершины, которые есть только как конечные точки рёбер
+            foreach (var vertexList in adjacencyList.Values)
             {
-                Console.Write($"{vertex.Key}: ");
-                foreach (var edge in vertex.Value)
+                foreach (var edge in vertexList)
                 {
-                    Console.Write($"({edge.Item1}, {edge.Item2}) ");
+                    if (!allVertices.Contains(edge.Item1))
+                    {
+                        allVertices.Add(edge.Item1);
+                    }
                 }
-                Console.WriteLine();
+            }
+
+            allVertices.Sort();
+
+            // Выводим список смежности для всех вершин
+            foreach (var vertex in allVertices)
+            {
+                Console.Write($"{vertex}: ");
+                if (adjacencyList.ContainsKey(vertex))
+                {
+                    foreach (var edge in adjacencyList[vertex])
+                    {
+                        Console.Write($"({edge.Item1}, {edge.Item2}) ");
+                    }
+                }
+                Console.WriteLine(); // Переход на новую строку после каждой вершины
             }
         }
     }
