fcb9af48e9555e3a279d130db7b0dc7a171e6c72
botya <danilk1102@gmail.com>
2024-09-29 19:23:55 +0300

    graph

---
+++ graph/Class1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace graph
+{
+    internal class Graph
+    {
+        private Dictionary<int, List<Tuple<int, int>>> adjacencyList;
+        private bool isDirected;
+        private bool isWeighted;
+        // Конструктор по умолчанию: создаёт пустой граф, ориентированный или неориентированный
+        public Graph(bool isDirected = true)
+        {
+            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            this.isDirected = isDirected;
+        }
+
+        public Graph(bool isDirected, bool isWeighted)
+        {
+            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            this.isDirected = isDirected;
+            this.isWeighted = isWeighted;
+        }
+
+        // Конструктор, заполняющий данные графа из файла
+        // Конструктор для графа из файла
+        public Graph(string input)
+        {
+            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+
+            using (StreamReader file = new StreamReader(input))
+            {
+                string firstLine = file.ReadLine();
+                if (firstLine == null)
+                {
+                    Console.WriteLine("Ошибка: Пустой файл.");
+                    return;
+                }
+
+                // Первая строка файла определяет тип графа: ориентированный или нет, взвешенный или нет
+                string[] graphTypeInfo = firstLine.Split(' ');
+                if (graphTypeInfo.Length != 2)
+                {
+                    Console.WriteLine("Ошибка: Некорректный формат первой строки. Ожидается 2 значения (ориентированность и взвешенность).");
+                    return;
+                }
+
+                // Ориентированный граф (y/n)
+                isDirected = graphTypeInfo[0].ToLower() == "y";
+
+                // Взвешенный граф (y/n)
+                isWeighted = graphTypeInfo[1].ToLower() == "y";
+
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var parts = line.Split(' ');
+
+                    if (parts.Length < 2)
+                    {
+                        Console.WriteLine($"Некорректная строка в файле: {line}");
+                        continue;
+                    }
+
+                    int startVertex = int.Parse(parts[0]);
+                    int endVertex = int.Parse(parts[1]);
+
+                    int weight = isWeighted ? int.Parse(parts[2]) : 1;
+
+                    // Добавляем ребро
+                    addEdge(startVertex, endVertex, weight);
+                }
+            }
+        }
+
+        // Конструктор-копия
+        public Graph(Graph otherGraph)
+        {
+            adjacencyList = new Dictionary<int, List<Tuple<int, int>>>();
+            this.isDirected = otherGraph.isDirected;
+            foreach (var vertex in otherGraph.adjacencyList)
+            {
+                adjacencyList[vertex.Key] = new List<Tuple<int, int>>(vertex.Value);
+            }
+        }
+
+        // Добавить ребро (дугу)
+        // Добавить ребро (дугу)
+        public void addEdge(int startVertex, int endVertex, int weight = 1)
+        {
+            if (!isWeighted)
+            {
+                weight = 1; // Если граф невзвешенный, устанавливаем вес в 1
+            }
+
+            if (!adjacencyList.ContainsKey(startVertex))
+            {
+                if (!adjacencyList.ContainsKey(endVertex))
+                {
+                    Console.WriteLine("Несуществующая конечная вершина");
+                }
+                else
+                {
+                    adjacencyList[startVertex] = new List<Tuple<int, int>>();
+                }
+            }
+            if (!adjacencyList.ContainsKey(endVertex))
+            {
+                Console.WriteLine("Несуществующая конечная вершина");
+            }
+            else
+            {
+                adjacencyList[startVertex].Add(new Tuple<int, int>(endVertex, weight));
+            }
+            
+            
+            // Если граф неориентированный, добавляем обратное ребро
+            if (!isDirected)
+            {
+                if (!adjacencyList.ContainsKey(endVertex))
+                {
+                    if (!adjacencyList.ContainsKey(endVertex))
+                    {
+                        Console.WriteLine("Несуществующая конечная вершина");
+                    }
+
+                    else
+                    {
+                        adjacencyList[endVertex] = new List<Tuple<int, int>>();
+                    }
+
+                }
+                if (!adjacencyList.ContainsKey(endVertex))
+                {
+                    Console.WriteLine("Несуществующая конечная вершина");
+                }
+                else
+                {
+                    adjacencyList[endVertex].Add(new Tuple<int, int>(startVertex, weight));
+                }
+            }
+        }
+
+
+        public void addEdge_const(int startVertex, int endVertex, int weight = 1)
+        {
+            if (!isWeighted)
+            {
+                weight = 1; // Если граф невзвешенный, устанавливаем вес в 1
+            }
+
+            if (!adjacencyList.ContainsKey(startVertex))
+            {
+                adjacencyList[startVertex] = new List<Tuple<int, int>>();
+            }
+            adjacencyList[startVertex].Add(new Tuple<int, int>(endVertex, weight));
+            // Если граф неориентированный, добавляем обратное ребро
+            if (!isDirected)
+            {
+                if (!adjacencyList.ContainsKey(endVertex))
+                {
+                    adjacencyList[endVertex] = new List<Tuple<int, int>>();
+                }
+                adjacencyList[endVertex].Add(new Tuple<int, int>(startVertex, weight));
+            }
+        }
+
+
+        // Добавить вершину
+        public void addVertex(int vertex, List<Tuple<int, int>> edges = null)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                adjacencyList[vertex] = edges ?? new List<Tuple<int, int>>();
+            }
+            else
+            {
+                Console.WriteLine($"Вершина {vertex} уже существует.");
+            }
+        }
+
+        
+
+        // Удаление ребра
+        public bool removeEdge(int startVertex, int endVertex, int weight)
+        {
+            if (adjacencyList.ContainsKey(startVertex))
+            {
+                Tuple<int, int> edge = new Tuple<int, int>(endVertex, weight);
+                bool removed = adjacencyList[startVertex].Remove(edge);
+
+                // Если граф неориентированный, удаляем и обратное ребро
+                if (!isDirected && removed)
+                {
+                    adjacencyList[endVertex].Remove(new Tuple<int, int>(startVertex, weight));
+                }
+
+                return removed;
+            }
+            return false;
+        }
+        // Удаление вершины
+        public bool removeVertex(int vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                Console.WriteLine($"Вершина {vertex} не найдена.");
+                return false;
+            }
+
+            adjacencyList.Remove(vertex);
+
+            // Удаляем все ребра, связанные с этой вершиной
+            foreach (var vertexEdges in adjacencyList.Values)
+            {
+                vertexEdges.RemoveAll(edge => edge.Item1 == vertex);
+            }
+
+            return true;
+        }
+
+        // Вывод списка смежности в файл
+        public void saveToFile(string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var vertex in adjacencyList)
+                {
+                    foreach (var edge in vertex.Value)
+                    {
+                        writer.WriteLine($"{vertex.Key} {edge.Item1} {edge.Item2}");
+                    }
+                }
+            }
+        }
+
+        // Вывод графа в консоль
+        public void printGraph()
+        {
+            foreach (var vertex in adjacencyList)
+            {
+                Console.Write($"{vertex.Key}: ");
+                foreach (var edge in vertex.Value)
+                {
+                    Console.Write($"({edge.Item1}, {edge.Item2}) ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ graph/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph
+{
+
+    internal class Program
+    {
+  
+
+
+        public static void Main(string[] args)
+        {
+            // Ввод пользователя: выбор создания графа с нуля или считывания из файла
+            Console.WriteLine("Вы хотите создать новый граф (1) или считать граф из файла (2)? Введите 1 или 2: ");
+            string choice = Console.ReadLine();
+
+            Graph graph = null;
+
+            if (choice == "1")
+            {
+                // Создание графа с нуля
+
+                // Ввод пользователя: выбор ориентированного или неориентированного графа
+                Console.WriteLine("Вы хотите работать с ориентированным графом? (y/n): ");
+                string directedChoice = Console.ReadLine();
+                bool isDirected = directedChoice.ToLower() == "y";
+
+                // Ввод пользователя: выбор взвешенного или невзвешенного графа
+                Console.WriteLine("Вы хотите работать с взвешенным графом? (y/n): ");
+                string weightedChoice = Console.ReadLine();
+                bool isWeighted = weightedChoice.ToLower() == "y";
+
+                // Создание пустого графа с заданными параметрами
+                graph = new Graph(isDirected, isWeighted);
+
+                // Добавляем вершины и ребра вручную (пример: добавим несколько ребер)
+                Console.WriteLine("Введите ребра в формате: начало конец (вес для взвешенных графов). Введите 'stop' для завершения:");
+                while (true)
+                {
+                    string edgeInput = Console.ReadLine();
+                    if (edgeInput.ToLower() == "stop") break;
+
+                    string[] edgeParts = edgeInput.Split(' ');
+                    if (edgeParts.Length < 2 || (isWeighted && edgeParts.Length != 3))
+                    {
+                        Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                        continue;
+                    }
+
+                    int startVertex = int.Parse(edgeParts[0]);
+                    int endVertex = int.Parse(edgeParts[1]);
+                    int weight = isWeighted ? int.Parse(edgeParts[2]) : 1;
+
+                    graph.addEdge_const(startVertex, endVertex, weight);
+                }
+            }
+            else if (choice == "2")
+            {
+                // Считывание графа из файла
+                Console.WriteLine("Введите имя файла для загрузки (например, graph_data.txt): ");
+                string fileName = Console.ReadLine();
+                string fullPath = Path.Combine("path_to_your_directory", fileName);
+
+                // Создание графа из файла
+                graph = new Graph(fullPath);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Программа завершена.");
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("1. Добавить вершину");
+                Console.WriteLine("2. Добавить ребро");
+                Console.WriteLine("3. Удалить вершину");
+                Console.WriteLine("4. Удалить ребро");
+                Console.WriteLine("5. Вывести граф");
+                Console.WriteLine("6. Сохранить в файл");
+                Console.WriteLine("0. Выйти");
+
+                string cases = Console.ReadLine();
+
+                switch (cases)
+                {
+                    case "1":
+                        Console.WriteLine("Введите номер вершины:");
+                        int vertex = int.Parse(Console.ReadLine());
+                        graph.addVertex(vertex);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Введите начальную вершину, конечную вершину и вес через пробел:");
+                        var edgeParts = Console.ReadLine().Split(' ');
+                        int start = int.Parse(edgeParts[0]);
+                        int end = int.Parse(edgeParts[1]);
+                        int weight = int.Parse(edgeParts[2]);
+                        graph.addEdge(start, end, weight);
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Введите номер вершины:");
+                        int removeVertex = int.Parse(Console.ReadLine());
+                        graph.removeVertex(removeVertex);
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Введите начальную вершину, конечную вершину и вес через пробел:");
+                        var removeEdgeParts = Console.ReadLine().Split(' ');
+                        int rStart = int.Parse(removeEdgeParts[0]);
+                        int rEnd = int.Parse(removeEdgeParts[1]);
+                        int rWeight = int.Parse(removeEdgeParts[2]);
+                        graph.removeEdge(rStart, rEnd, rWeight);
+                        break;
+
+                    case "5":
+                        graph.printGraph();
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Введите путь к файлу для сохранения:");
+                        string filePath = Console.ReadLine();
+                        graph.saveToFile(filePath);
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор.");
+                        break;
+                }
+            }
+        }
+    }
+}
